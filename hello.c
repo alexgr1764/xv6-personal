c05851fe5393b1615877686624506266d923f922
alexgr1764 <alejandroguerrero.r97@gmail.com>
2023-09-22 23:57:07 -0600

    hw changes

---
+++ hello.c
@@ -0,0 +1,12 @@
+// Commented lines are for xv6 or linux, respectively
+// #include "user.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+
+int main()
+{
+    // printf("Hello, world!\n");
+    printf(2, "Hello, world!\n");
+    exit();
+    // exit(0);
+}
